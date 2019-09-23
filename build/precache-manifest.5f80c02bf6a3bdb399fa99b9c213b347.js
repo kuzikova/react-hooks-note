26f0baab09ffc69079a12c1a998fbeba022163d7
catjon <htmlcat5@gmail.com>
2019-09-24 01:14:48 +0300

    first commit

---
+++ build/precache-manifest.5f80c02bf6a3bdb399fa99b9c213b347.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ba0ca05b056ebe10bfc4c1fe3a7763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "497b2b32fa579aed55c3",
+    "url": "/static/css/main.505eb550.chunk.css"
+  },
+  {
+    "revision": "85f9225601f33d6a2e18",
+    "url": "/static/js/2.125903da.chunk.js"
+  },
+  {
+    "revision": "497b2b32fa579aed55c3",
+    "url": "/static/js/main.f65530bc.chunk.js"
+  },
+  {
+    "revision": "017d3b11e29cdc5d4f13",
+    "url": "/static/js/runtime-main.6ac1081b.js"
+  }
+]);
